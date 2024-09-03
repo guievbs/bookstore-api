8f0f68a817c263392d554636c257b88d057e31ea
Guilherme Sebastião <165939012+guievbs@users.noreply.github.com>
2024-09-03 14:56:51 -0300

    feat: adiciona o middleware de autenticação

---
+++ middleware/authMiddleware.js
@@ -0,0 +1,22 @@
+const jwt = require("jsonwebtoken");
+
+exports.authMiddleware = (req, res, next) => {
+  const token = req.header("Authorization");
+  if (!token)
+    return res.status(401).json({ message: "No token, authorization denied" });
+
+  try {
+    const decoded = jwt.verify(token.split(" ")[1], process.env.JWT_SECRET);
+    req.user = decoded;
+    next();
+  } catch (err) {
+    res.status(401).json({ message: "Token is not valid" });
+  }
+};
+
+exports.adminMiddleware = (req, res, next) => {
+  if (req.user.role !== "admin") {
+    return res.status(403).json({ message: "Access denied" });
+  }
+  next();
+};
