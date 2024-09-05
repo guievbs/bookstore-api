7c6c9dc0989368a162c5a0c4bc6b0bfdbdc651a3
Guilherme Sebastião <165939012+guievbs@users.noreply.github.com>
2024-09-05 12:26:27 -0300

    refactor: nova verficação de admin

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -14,9 +14,24 @@ exports.authMiddleware = (req, res, next) => {
   }
 };
 
-exports.adminMiddleware = (req, res, next) => {
-  if (req.user.role !== "admin") {
-    return res.status(403).json({ message: "Access denied" });
-  }
-  next();
+exports.verifyAdmin = (req, res, next) => {
+  const token = req.headers.authorization?.split(" ")[1];
+
+  if (!token) return res.status(401).json({ error: "Token não fornecido" });
+
+  jwt.verify(token, process.env.JWT_SECRET, (err, decoded) => {
+    if (err)
+      return res.status(401).json({ error: "Token inválido ou expirado" });
+
+    req.user = decoded;
+
+    if (req.user.role !== "admin") {
+      return res.status(403).json({
+        error:
+          "Acesso negado. Somente administradores podem acessar esta rota.",
+      });
+    }
+
+    next();
+  });
 };
